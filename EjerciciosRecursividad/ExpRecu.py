ba3b9f66ce5a2b4228d5aa19468e923a3d43eec2
Daniel Ortiz <36578005+ZitroDHOR@users.noreply.github.com>
2018-03-06 11:48:07 -0500

    Update ExpRecu.py

--- EjerciciosRecursividad/ExpRecu.py
+++ EjerciciosRecursividad/ExpRecu.py
@@ -3,7 +3,3 @@ def expRecu (base,expo):
         return 1;
     else:
         return base * expRecu(base,expo-1);
-
-base=int(input("\n Ingrese la base:"))
-expo=int(input("\n Ingrese el exponente:"))
-print("El vlaor es"+str(expRecu(base,expo)));
